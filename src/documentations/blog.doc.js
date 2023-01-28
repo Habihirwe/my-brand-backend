a595854d2067e2b7d2a7d6dd2fcc85531ef8080d
Habihirwe <habiholivier10@gmail.com>
2023-01-28 17:26:06 +0200

    feat(blog-swagger-documentation): -create swaggerdoc.js create documentation folder -create blog.doc.js -import blogRouteDocs in swaggerdoc.js -modify package.json -modify server.js [finish#005]

---
+++ src/documentations/blog.doc.js
@@ -0,0 +1,202 @@
+const listAllBlogs = {
+    tags:['Blogs'],
+    description:"List all Blogs",
+    responses:{
+        200:{
+            description:"OK",
+            content:{
+                 "application/json":{
+                    type:'object',
+                    example:{
+                        status:"success",
+                        data:[]
+                    }
+                 }
+            }
+        }
+    }
+}
+
+const getBlogById = {
+    tags:['Blogs'],
+    description:"Get blog by id",
+    parameters:[
+        {
+            name:"id",
+            in:"path",
+            description:"id of the blog",
+            type:"string",
+            example:"63caaf3527b29e1d399896da"
+        }
+    ],
+    responses:{
+        200:{
+            description:"OK",
+            content:{
+                 "application/json":{
+                    type:'object',
+                    example:{
+                        status:"success",
+                        data:[]
+                    }
+                 }
+            }
+        }
+    }
+}
+
+const createBlog = {
+    tags:['Blogs'],
+    description:"Create a Blog post",
+    security: [
+        {
+          auth_token: [],
+        },
+      ],
+    requestBody:{
+        content:{
+            "multipart/form-data":{
+                schema:{
+                    type:"object",
+                    properties:{
+
+                        image:{
+                            type:"file",
+                            description:"the image of the blog post"
+                        },
+                        title:{
+                            type:"string",
+                        },
+                        description:{
+                            type:"string",
+                        },
+                       
+                    }
+                }
+            }
+        }
+    },
+    responses:{
+        201:{
+            description:"OK",
+            content:{
+                "application/json":{
+                    type:"object",
+                    example:{
+                        status:"success",
+                        data:[]
+                    }
+                }
+            }
+        }
+    }
+}
+
+const deleteBlogPost = {
+    tags:['Blogs'],
+    description:"Delete the blog post by id",
+    security: [
+        {
+          auth_token: [],
+        },
+      ],
+    parameters:[
+        {
+            name:"id",
+            in:"path",
+            description:"id of the blog",
+            type:"string"
+        }
+    ],
+
+    responses:{
+        200:{
+            description:"OK",
+            content:{
+                 "application/json":{
+                    type:'object',
+                    example:{
+                        status:"success",
+                        data:[]
+                    }
+                 }
+            }
+        }
+    }
+}
+
+const updateBlogPost = {
+    tags:['Blogs'],
+    description:"Update a Blog post",
+    security: [
+        {
+          auth_token: [],
+        },
+      ],
+    parameters:[
+        {
+            name:"id",
+            in:"path",
+            description:"id of the blog",
+            type:"string"
+        }
+    ],
+    requestBody:{
+        content:{
+            "multipart/form-data":{
+                schema:{
+                    type:"object",
+                    properties:{
+                        image:{
+                            type:"file",
+                            description:"the image of the blog post"
+                        },
+                        title:{
+                            type:"string",
+                        },
+                        description:{
+                            type:"string",
+                        },
+                       
+                    }
+                }
+            }
+        }
+    },
+    responses:{
+        201:{
+            description:"OK",
+            content:{
+                "application/json":{
+                    type:"object",
+                    example:{
+                        status:"success",
+                        data:[]
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+ const blogRouteDocs = {
+    "/api/getAllBlogs":{
+        get:listAllBlogs
+    },
+    "/api/getSingleBlog/{id}":{
+        get:getBlogById
+    },
+    "/api/createBlog":{
+        post:createBlog
+    },
+    "/api/deleteBlog/{id}":{
+        delete:deleteBlogPost
+    },
+    "/api/updatePost/{id}":{
+        put:updateBlogPost
+    },
+}
+
+export default blogRouteDocs
---
+++ src/documentations/swaggerdoc.js
@@ -0,0 +1,59 @@
+import blogRouteDocs from "./blog.doc"
+
+
+const swaggerDocumentations ={
+    
+        openapi: "3.0.0",
+        info:{
+            title: "My brand ",
+            version:"0.1.0",
+            description:"This is the backend of my brand"
+        },
+       
+        servers:[
+            {
+                url: "http://localhost:3000",
+                name:"development server"
+            },
+        ],
+
+
+        components: {
+            securitySchemes: {
+              auth_token: {
+                type: 'apiKey',
+                name: 'auth_token',
+                in: 'header'
+              }
+            }
+          },
+          security: [
+            {
+              auth_token: []
+            }
+          ],
+        
+        tags:[
+            {
+                name:"Blogs",
+                description:""
+            },
+            {
+              name:"Users",
+              description:""
+          },
+          {
+            name:"Queries",
+            description:""
+        },
+    
+
+        ],
+        paths:{
+          ...blogRouteDocs
+         
+
+        }
+    }
+
+export default swaggerDocumentations
